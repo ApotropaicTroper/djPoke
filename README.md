e786886652ecccd9cca0c416b9798efe32a5452d
Zoidmania <zoidmania.leland@gmail.com>
2016-07-13 20:50:31 -0400

    Updated README.md

--- README.md
+++ README.md
@@ -7,3 +7,8 @@ I'm new to Django. This is my repo for learning it.
 
 ## What's the end result?
 When this is done, this will essentially be a clone of Bulbapedia (or other tools like it).
+
+## screenscraper.py?
+This script culls data about Pokemon from Bulbapedia using BeautifulSoup4.
+At the moment, it only generates the list of pokemon; no data is being siphoned
+quite yet.
--- screenscraper.py
+++ screenscraper.py
@@ -1,31 +1,50 @@
 from bs4 import BeautifulSoup as bs
 from urllib2 import urlopen as wget
 
-# first, we get the whole list of pokemon, sorted by national dex number. there is also a regional dex number, which i will preserve later.
-soup = bs(wget("http://bulbapedia.bulbagarden.net/wiki/List_of_Pok%C3%A9mon_by_National_Pok%C3%A9dex_number").read(), 'lxml') # gets rid of implicit parser warning
-tables = soup.findAll('table', {'align':'center'})
+URL = "http://bulbapedia.bulbagarden.net"
 
-# tables[0] is the list of kanto (kdex) pokemon.
-# tables[1] is the list of jhoto (jdex) pokemon.
-# tables[2] is the list of hoenn (hdex) pokemon.
-# tables[3] is the list of sinnoh (sdex) pokemon.
-# tables[4] is the list of unova (udex) pokemon.
-# tables[5] is the list of kalos pokemon. kalos is special because the region is split into 3 sub regions, central (cekdex), coastal (cokdex), and mountain (mokdex).
-# tables[6] is the lsit of alola (adex) pokemon. it is not populated, as the region is part of the gen VII game release (not released yet).
+def populateList():
+    '''first, we get the whole list of pokemon, sorted by national dex number.
+    there is also a regional dex number, which i will preserve later.
+    returns a tuple in the form (name, url_suffix).
+    '''
+    path = "http://bulbapedia.bulbagarden.net/wiki/List_of_Pok%C3%A9mon_by_National_Pok%C3%A9dex_number"
+    soup = bs(wget(path).read(), 'lxml')
+    tables = soup.findAll('table', {'align':'center'})
 
-# get a list of pokemon
-pokemon = []
-for table in tables[:6]:
-    entries = bs(table.__str__(), 'lxml').findAll('tr')
-    for entry in entries[1:]:   # entries[0] defines column headers.
-        entry = bs(entry.__str__(), 'lxml')
-        info = entry.findAll('td')[3]
-        poke = (info.a.contents[0], info.a['href'])
-        if poke not in pokemon: # there are duplicate entries. some pokemon have different "states", and as such have multiple entries linking to a single page.
-            pokemon.append(poke)    # using a dictionary reorders, lets stay in order for debugging's sake.
+    # - tables[0] is the list of kanto (kdex) pokemon.
+    # - tables[1] is the list of jhoto (jdex) pokemon.
+    # - tables[2] is the list of hoenn (hdex) pokemon.
+    # - tables[3] is the list of sinnoh (sdex) pokemon.
+    # - tables[4] is the list of unova (udex) pokemon.
+    # - tables[5] is the list of kalos pokemon. kalos is special because the region is 
+    #   split into 3 sub regions, central (cekdex), coastal (cokdex), and mountain (mokdex).
+    # - tables[6] is the list of alola (adex) pokemon. it is not populated, as the region 
+    #   is part of the gen VII game release (not released yet).
 
-for item in pokemon:
-    print item
-print 'length:', len(pokemon)
+    # get a list of pokemon
+    pokemon = []
+    for table in tables[:6]:    # ignoring alola region for now
+        entries = bs(table.__str__(), 'lxml').findAll('tr')
+        for entry in entries[1:]:   # entries[0] defines column headers.
+            entry = bs(entry.__str__(), 'lxml')
+            info = entry.findAll('td')[3]
+            poke = (info.a.contents[0], info.a['href'])
+            if poke not in pokemon:     # there are duplicate entries. some pokemon have different "states".
+                pokemon.append(poke)    # using a dictionary reorders, lets stay in order for debugging's sake.
 
-# so far, only gets a list of Kanto Pokemon. Working on the rest.
+    # for item in pokemon:
+    #     print item
+    # print 'length:', len(pokemon)
+    return pokemon
+
+def cullPokemonData(id):
+    path = URL + id[1]
+    soup = bs(wget(path).read(), 'lxml')
+
+pokedex = populateList()
+cullPokemonData(pokedex[0])
+
+# for pokemon in pokedex:
+#     cullPokemonData(pokemon)
+#     break
