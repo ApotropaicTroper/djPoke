88ba4da46840a2ba4b05901425b178089a5beef1
Leland Sperl <zoidmania.leland@gmail.com>
2016-07-19 01:21:58 -0400

    patched in MariaDB hooks, started cullPokemonData()

--- README.md
+++ README.md
@@ -5,15 +5,23 @@ Not much, how about you?
 
 The following is information about what to do with this information.
 
-# The Python Wheels
+## The Python Wheels
 Install them.
 
-# SQL Connector?
+## SQL Connector?
 Install it. It's necessary before you install mysql-python from pip.
 
-# The Scrubber Script
+## The Scrubber Script
 Run it.
 
-# Anything else?
-The script depends on MariaDB 10.14. Install that, and, since MariaDB is essentially a *fork*, you can 
-`pip install mysql-python` it, as long as you've already installed MySQL-Connector/C 6.0.2.
+## Anything else?
+The script depends on MariaDB 10.14. Install that, and, since MariaDB is essentially a *fork* of MySQL, you can 
+`pip install mysql-python` it, as long as you've already installed MySQL-Connector/C 6.0.2 32-bit.
+
+## Dependencies
+
+- Python             2.7.11  32-bit
+- MariaDB            10.1    32-bit
+- MySql Connector/C  6.0.2   32-bit
+- BeautifulSoup      4.4     (from pip)
+- MySQl-Python       1.2.5   (from pip)
--- pokemon-screenscraper.py
+++ pokemon-screenscraper.py
@@ -1,70 +1,97 @@
 from bs4 import BeautifulSoup as bs
 from urllib2 import urlopen as wget
+import MySQLdb as sql
+import sys
 
 URL = "http://bulbapedia.bulbagarden.net"
+db = sql.connect("localhost","root","Metroid","pokedb" )
+cursor = db.cursor()
 
-def initDB():
-    import sqlite3
-    sqlite_file = 'pokebase.sqlite3'    # name of the sqlite database file
-    # Connecting to the database file
-    conn = sqlite3.connect(sqlite_file)
-    c = conn.cursor()
+def initDB():    
     scripts = [
-        "CREATE TABLE IF NOT EXISTS pokemon( id TEXT PRIMARY KEY, name TEXT, category TEXT, natdex INTEGER, type TEXT, region TEXT);",
-        "SELECT name FROM sqlite_master WHERE type='table';",
+        "DROP TABLE pokemon;",
+        "CREATE TABLE pokemon( id serial, name CHAR(30) PRIMARY KEY, category CHAR(50), natdex INTEGER, type CHAR(30), region CHAR(30));",
     ]
-    # Creating a new SQLite table with 1 column
+    
     for script in scripts:
-        c.execute(script)
-    print(c.fetchall())
-    # Committing changes and closing the connection to the database file
-    conn.commit()
-    conn.close()
+        cursor.execute(script)
+        out = cursor.fetchone()
+        if out:
+            print out
+
+    return cursor
 
 def populateList():
     '''first, we get the whole list of pokemon, sorted by national dex number.
     there is also a regional dex number, which i will preserve later.
     returns a tuple in the form (name, url_suffix).
     '''
-    path = "http://bulbapedia.bulbagarden.net/wiki/List_of_Pok%C3%A9mon_by_National_Pok%C3%A9dex_number"
-    soup = bs(wget(path).read(), 'lxml')
-    tables = soup.findAll('table', {'align':'center'})
+    path = URL + "/wiki/List_of_Pok%C3%A9mon_by_National_Pok%C3%A9dex_number"
+    page = wget(path)
+    soup = bs(page.read(), 'html.parser')
+    tables = soup.findAll('table')
 
-    # - tables[0] is the list of kanto (kdex) pokemon.
-    # - tables[1] is the list of jhoto (jdex) pokemon.
-    # - tables[2] is the list of hoenn (hdex) pokemon.
-    # - tables[3] is the list of sinnoh (sdex) pokemon.
-    # - tables[4] is the list of unova (udex) pokemon.
-    # - tables[5] is the list of kalos pokemon. kalos is special because the region is 
-    #   split into 3 sub regions, central (cekdex), coastal (cokdex), and mountain (mokdex).
-    # - tables[6] is the list of alola (adex) pokemon. it is not populated, as the region 
-    #   is part of the gen VII game release (not released yet).
+    # - tables[1] is the list of kanto (kdex) pokemon.
+    # - tables[2] is the list of jhoto (jdex) pokemon.
+    # - tables[3] is the list of hoenn (hdex) pokemon.
+    # - tables[4] is the list of sinnoh (sdex) pokemon.
+    # - tables[5] is the list of unova (udex) pokemon.
+    # - tables[6] is the list of kalos pokemon. kalos is special because the region is 
+    #     split into 3 sub regions, central (cekdex), coastal (cokdex), and mountain (mokdex).
+    # - tables[7] is the list of alola (adex) pokemon. it is not populated, as the region 
+    #     is part of the gen VII game release (not released yet).
 
     # get a list of pokemon
     pokemon = []
-    for table in tables[:6]:    # ignoring alola region for now
-        entries = bs(table.__str__(), 'lxml').findAll('tr')
+    for table in tables[:7]:    # ignoring alola region for now
+        entries = bs(table.__str__(), 'html.parser').findAll('tr')
         for entry in entries[1:]:   # entries[0] defines column headers.
-            entry = bs(entry.__str__(), 'lxml')
+            entry = bs(entry.__str__(), 'html.parser')
             info = entry.findAll('td')[3]
             poke = (info.a.contents[0], info.a['href'])
             if poke not in pokemon:     # there are duplicate entries. some pokemon have different "states".
                 pokemon.append(poke)    # using a dictionary reorders, lets stay in order for debugging's sake.
 
-    # for item in pokemon:
-    #     print item
-    # print 'length:', len(pokemon)
     return pokemon
 
-def cullPokemonData(id):
-    path = URL + id[1]
-    soup = bs(wget(path).read(), 'lxml')
+def cullPokemonData(pokeTuple):
+    path = URL + pokeTuple[1]
+    page = wget(path)
+    soup = bs(page.read(), 'html.parser')
+    table = soup.find('table', {'class':'roundy', 'style': 'float:right; text-align:center; width:33%; max-width:420px; min-width:360px; background: #78C850; border: 2px solid #682A68; padding:2px;'})
 
+    name     = ""
+    category = ""
+    natdex   = ""
+    _type    = ""
+    region   = ""
+
+    # script = "INSERT INTO pokemon(name, category, natdex, type, region) VALUES ()"
+    # try:
+    #     cursor.execute(script)
+    #     out = cursor.fetchone()
+    #     if out:
+    #         print out
+    #     db.commit()
+    # except:
+    #     db.rollback()
+
+
+# ------ BEGIN ROUTINE -------
+sys.stdout.write("Initializing Database...")
+cursor = initDB()
+
+sys.stdout.write("done.\nGetting list of Pokemon (up to Gen6)...")
 pokedex = populateList()
-#cullPokemonData(pokedex[0])
-initDB()
 
+print "done.\nStarting Database Insertions."
 
-# for pokemon in pokedex:
+sys.stdout.write("Processing " + pokedex[0][0] + "...") # separate the first one for console output's sake
+cullPokemonData(pokedex[0])
+# for pokemon in pokedex[1:]:
+#     sys.stdout.write("done.\nProcessing " + pokemon[0] + "...")
 #     cullPokemonData(pokemon)
-#     break
+
+print "done.\nDatabase construction complete."
+# cleanup
+db.close()
