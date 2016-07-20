fd6180ce650c9cba92b824d8e1ca57307c77e6da
Leland Sperl <zoidmania.leland@gmail.com>
2016-07-20 02:20:16 -0400

    added todo for scraper script:

--- pokemon-screenscraper.py
+++ pokemon-screenscraper.py
@@ -164,3 +164,46 @@ last = 'Routine complete in {0:.2f} seconds.'.format(time.time()-start)
 for i in range(len(last)):
     sys.stdout.write("=")
 print "\n" + last
+
+# maybe do this for a percentage completion updater in the console:
+"""
+from threading import Timer
+
+class RepeatedTimer(object):
+    def __init__(self, interval, function, *args, **kwargs):
+        self._timer     = None
+        self.interval   = interval
+        self.function   = function
+        self.args       = args
+        self.kwargs     = kwargs
+        self.is_running = False
+        self.start()
+
+    def _run(self):
+        self.is_running = False
+        self.start()
+        self.function(*self.args, **self.kwargs)
+
+    def start(self):
+        if not self.is_running:
+            self._timer = Timer(self.interval, self._run)
+            self._timer.start()
+            self.is_running = True
+
+    def stop(self):
+        self._timer.cancel()
+        self.is_running = False
+
+from time import sleep
+
+def hello(name):
+    print "Hello %s!" % name
+
+print "starting..."
+rt = RepeatedTimer(1, hello, "World") # it auto-starts, no need of rt.start()
+try:
+    sleep(5) # your long-running job goes here...
+finally:
+    rt.stop() # better in a try/finally block to make sure the program ends!
+"""
+
